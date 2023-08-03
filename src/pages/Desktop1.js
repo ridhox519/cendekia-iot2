a97332276cd4b78b669e73b8cf3b7996b85ee888
ridhox519 <141166915+ridhox519@users.noreply.github.com>
2023-08-03 20:25:59 +0700

    Add files via upload

---
+++ src/pages/Desktop1.js
@@ -0,0 +1,167 @@
+import "./Desktop1.css";
+const Desktop1 = () => {
+  return (
+    <div className="desktop-1">
+      <div className="homepage-iot" />
+      <div className="desktop-1-child" />
+      <img className="desktop-1-item" alt="" src="/rectangle-21@2x.png" />
+      <img
+        className="logo-cendekiaiot-2-logo-putih"
+        alt=""
+        src="/logocendekiaiot-2-logo-putih-1@2x.png"
+      />
+      <b className="home">Home</b>
+      <b className="our-stories">Our Stories</b>
+      <b className="en">En</b>
+      <img className="desktop-1-inner" alt="" src="/polygon-1.svg" />
+      <b className="we-simplify-the-container">
+        <span>{`We `}</span>
+        <span className="simplify">simplify</span>
+        <span> the Internet of Things for you.</span>
+      </b>
+      <div className="a-single-application">
+        A single application for prototyping, deploying, and remotely managing
+        interconnected electronic devices on a scalable level
+      </div>
+      <img className="group-icon" alt="" src="/group-3.svg" />
+      <b className="developers">Developers</b>
+      <b className="industries">Industries</b>
+      <div className="group-parent">
+        <div className="rectangle-wrapper">
+          <div className="group-child" />
+        </div>
+        <div className="learn-more">{`Learn More >>`}</div>
+      </div>
+      <div className="rectangle-div" />
+      <img className="rectangle-icon" alt="" src="/rectangle-23@2x.png" />
+      <img className="desktop-1-child1" alt="" src="/rectangle-24@2x.png" />
+      <div className="desktop-1-child2" />
+      <div className="desktop-1-child3" />
+      <img className="desktop-1-child4" alt="" src="/group-7.svg" />
+      <div className="group-container">
+        <div className="rectangle-wrapper">
+          <div className="group-child" />
+        </div>
+        <div className="learn-more">{`Pricing >>`}</div>
+      </div>
+      <div className="group-div">
+        <div className="rectangle-wrapper">
+          <div className="group-child" />
+        </div>
+        <div className="contact-sales">{`Contact Sales>>`}</div>
+      </div>
+      <b className="our-products">
+        <span>Our</span>
+        <span className="span">{` `}</span>
+        <span className="simplify">Products!</span>
+      </b>
+      <img
+        className="logo-cendekiaiot-2-logo-putih2"
+        alt=""
+        src="/logocendekiaiot-2-logo-putih2-1@2x.png"
+      />
+      <div className="cendekia-iot">
+        <span>{`Cendekia `}</span>
+        <span className="simplify">IoT</span>
+      </div>
+      <div className="cendekia-iot-is-container">
+        <span className="cendekia-iot-is-container1">
+          <p className="cendekia-iot-is-your-gateway-t">
+            <b className="cendekia-iot1">{`Cendekia IoT `}</b>
+            <span className="is-your-gateway">{`is your gateway to a connected and smarter future! As a leading service provider in the realm of Internet of Things, we are dedicated to unraveling the potential of IoT and transforming challenges into innovative solutions. `}</span>
+          </p>
+          <p className="imagine-a-future">
+            Imagine a future where everything is interconnected - from homes and
+            cities to industries and healthcare!
+          </p>
+        </span>
+      </div>
+      <div className="offering-all-the">
+        Offering all the necessary functionalities for creating and overseeing
+        connected hardware. This suite includes device setup, sensor data
+        representation, mobile and web-based remote control, secure cloud
+        services, Over-The-Air firmware updates, data analysis, user and access
+        administration, alerts, automations, and numerous other features.
+      </div>
+      <b className="a-fully-integrated-container">
+        <span>A</span>
+        <span className="span1">{` `}</span>
+        <span className="simplify">fully integrated</span>
+        <span> set of IoT software is available</span>
+      </b>
+      <div className="discover-cendekia-iots-container">
+        <p className="discover-cendekia-iots">{`Discover Cendekia IoT's range of products and features, or sign up now to commence building your first IoT product. `}</p>
+        <p className="additionally-feel-free">
+          Additionally, feel free to reach out to us for a tailored solution
+          that suits your business needs.
+        </p>
+      </div>
+      <div className="cendekia-internet-of">
+        © 2023 Cendekia Internet of Things. All Rights Reserved.
+      </div>
+      <b className="no-more-scrolling-container">
+        <span>{`No more scrolling. Let's `}</span>
+        <span className="simplify">begin!</span>
+      </b>
+      <b className="smart-aquaponics">Smart Aquaponics</b>
+      <div className="desktop-1-inner1">
+        <div className="group-child1" />
+      </div>
+      <div className="desktop-1-inner2">
+        <div className="group-child1" />
+      </div>
+      <div className="desktop-1-inner3">
+        <div className="group-child1" />
+      </div>
+      <b className="wilmos">WILMOS</b>
+      <b className="hydropro">HYDROPRO</b>
+      <b className="smart-water-dispenser">Smart Water Dispenser</b>
+      <b className="icams">ICAMS</b>
+      <b className="smart-fish-feeder">Smart Fish Feeder</b>
+      <div className="hydropro1">HYDROPRO</div>
+      <div className="group-parent1">
+        <div className="rectangle-wrapper1">
+          <div className="group-child1" />
+        </div>
+        <img className="favicon2-1" alt="" src="/favicon2-1@2x.png" />
+      </div>
+      <div className="group-parent2">
+        <div className="rectangle-wrapper1">
+          <div className="group-child1" />
+        </div>
+        <img className="favicon-3" alt="" src="/favicon-3@2x.png" />
+      </div>
+      <div className="group-parent3">
+        <div className="rectangle-wrapper1">
+          <div className="group-child1" />
+        </div>
+        <img className="favicon-4" alt="" src="/favicon-4@2x.png" />
+      </div>
+      <b className="creative-intuitive-container">
+        <span>{`Creative & Intuitive `}</span>
+        <span className="design">Design</span>
+      </b>
+      <div className="user-friendly-apps">USER-FRIENDLY APPS</div>
+      <b className="benefits-both-your-container">
+        <span>{`Benefits `}</span>
+        <span className="design">both your team and your customers</span>
+      </b>
+      <div className="cendekia-iot-empowers-container">
+        <span>
+          <span className="cendekia-iot2">Cendekia IoT</span>
+          <span className="span2">{` `}</span>
+        </span>
+        <span className="empowers-companies-to">
+          empowers companies to bring their electronic products online and
+          create customer-facing applications without the need for coding. This
+          enables remote control and management of these devices, allowing for
+          seamless interactions with customers
+        </span>
+      </div>
+      <img className="mobile-1-icon" alt="" src="/mobile-1@2x.png" />
+      <img className="mobile-2-1" alt="" src="/mobile-2-1@2x.png" />
+    </div>
+  );
+};
+
+export default Desktop1;
